296beced1f692696569b6d7d71e83d071e95e32e
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-02-16 08:24:22 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char a;
@@ -14,5 +19,5 @@ int main(void)
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(_myage));
 	printf("Size of a float: %ld byte(s)\n", sizeof(salary));
 
-	return(0);
+	return (0);
 }
