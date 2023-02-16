53cc2669e522f235373dd2dabe1ac071e8ef3d2c
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-02-16 09:01:13 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -9,5 +10,5 @@
 int main(void)
 {
 	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return 1;
+	return (1);
 }
