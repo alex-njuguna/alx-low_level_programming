4b62d4e23dad81b1b8a42292f85446c74eb113e1
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-02-16 08:26:02 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	float salary;
 
 	printf("Size of a char: %ld byte(s)\n", sizeof(a));
-	printf("Size of a int: %ld byte(s)\n", sizeof(age));
+	printf("Size of an int: %ld byte(s)\n", sizeof(age));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(myage));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(_myage));
 	printf("Size of a float: %ld byte(s)\n", sizeof(salary));
