2e42928c857c1d3764bd46a419ef5008e6690e99
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-02-16 09:18:27 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * main - prints the standrad error sandwiched around the  standard outpu/t
  *
- * Return 1
+ * Return: returns 1
  */
-
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
