66a94f3029d9e08955d25573d05377ca94547303
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-02-17 07:10:36 -0800

    task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * return: 0
+ *
+ */
+int main(void)
+{
+	int num1, num2, num3;
+
+	for (num1 = 48; num1 < 58; num1++)
+	{
+		for(num2 = 48; num2 < 58; num2++)
+		{
+			for (num3 = 48; num3 <58; num3++)
+			{
+				if (num1 < num2 && num2 < num3 && num1 < num3)
+				{
+					putchar(num1);
+					putchar(num2);
+					putchar(num3);
+					if (num1 == 55 && num2 == 56 && num3 == 57)
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
