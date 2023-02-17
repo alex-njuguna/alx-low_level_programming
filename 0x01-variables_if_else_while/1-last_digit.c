4ade5727fc54a183511590073f2e55c20f78e273
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-02-17 03:35:57 -0800

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,8 +26,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not
-                                0\n", n, last_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 	}
 	return (0);
 }
