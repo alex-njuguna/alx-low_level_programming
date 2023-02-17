c6ffb316244a301a87fdbf962dbd65028327b733
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-02-16 22:30:49 -0800

    task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*int main: Entry
+ *
+ * Return: return 0
+ *
+ */
+int main(void)
+{
+	if (n > 0)
+		printf("is positive\n");
+	else if (n == 0)
+		print("is zero\n");
+	else
+		print("is negative");
+	return (0);
+}
