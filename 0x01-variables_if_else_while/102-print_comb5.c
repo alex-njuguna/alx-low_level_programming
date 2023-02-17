7c83beb42d8577f436da550383ccdfac64018c43
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-02-17 07:33:48 -0800

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,18 +8,30 @@
  */
 int main(void)
 {
-	int num1, num2;
+	int num1, num2, num3, num4;
 
-	for (num1 = 48; num1 < 148; num1++)
+	for (num1 = 48; num1 < 58; num1++)
 	{
-		for (num2 = 48; num2 < 148; num2++)
+		for (num2 = 48; num2 < 58; num2++)
 		{
-			putchar(num1);
-			putchar(num2);
-			if (num1 == 147 && num2 == 147)
-				break;
-			putchar(',');
-			putchar(' ');
+			for (num3 = 48; num3 < 58; num3++)
+			{
+				for (num4 = 48; num4 < 58; num4++)
+				{
+					if (num1 < num2 && num1 < num3 && num1 < num4 && num2 < num3 && num2 < num4 && num3 < num4)
+					{
+						putchar(num1);
+						putchar(num2);
+						putchar(' ');
+						putchar(num3);
+						putchar(num4);
+						if (num1 == 57 && num2 == 56 && num3 == 57 && num4 == 57)
+							break;
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
 		}
 	}
 	putchar('\n');
