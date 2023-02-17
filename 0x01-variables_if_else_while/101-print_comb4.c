9aac0db7fbe89cdcc47a1a7b0142e95f3d807b54
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-02-17 07:12:19 -0800

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,9 +12,9 @@ int main(void)
 
 	for (num1 = 48; num1 < 58; num1++)
 	{
-		for(num2 = 48; num2 < 58; num2++)
+		for (num2 = 48; num2 < 58; num2++)
 		{
-			for (num3 = 48; num3 <58; num3++)
+			for (num3 = 48; num3 < 58; num3++)
 			{
 				if (num1 < num2 && num2 < num3 && num1 < num3)
 				{
