3938b794ec3b716d3e5e26af65567fa90f55b70b
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-02-18 11:20:16 -0800

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,34 +8,34 @@
  */
 int main(void)
 {
-	int num1, num2, num3, num4, sum1, sum2;
+int num1, num2, num3, num4, sum1, sum2;
 
-	for (num1 = 48; num1 < 58; num1++)
+for (num1 = 48; num1 < 58; num1++)
+{
+	for (num2 = 48; num2 < 58; num2++)
 	{
-		for (num2 = 48; num2 < 58; num2++)
+		for (num3 = 48; num3 < 58; num3++)
 		{
-			for (num3 = 48; num3 < 58; num3++)
+			for (num4 = 48; num4 < 58; num4++)
 			{
-				for (num4 = 48; num4 < 58; num4++)
+				sum1 = (num1 * 10) + num2;
+				sum2 = (num3 * 10) + num4;
+				if (sum1 < sum2)
 				{
-					sum1 = (num1 * 10) + num2;
-					sum2 = (num3 * 10) + num4;
-					if (sum1 < sum2)
-					{
-						putchar(num1);
-						putchar(num2);
-						putchar(' ');
-						putchar(num3);
-						putchar(num4);
-						if (num1 == 57 && num2 == 56 && num3 == 57 && num4 == 57)
-							break;
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(num1);
+					putchar(num2);
+					putchar(' ');
+					putchar(num3);
+					putchar(num4);
+					if (num1 == 57 && num2 == 56 && num3 == 57 && num4 == 57)
+						break;
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
 	}
-	putchar('\n');
-	return (0);
+}
+putchar('\n');
+return (0);
 }
