aa81eb0e776d6b567b2d30e0873c22b267e0063c
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-02-21 09:09:27 -0800

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print all natural numbers to 98
+ * @n: starting point
+ * Return: nada
+ *
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			_putchar(i);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else if ( n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			_putchar(i);
+			_putchar(',');
+                        _putchar(' ');
+		}
+	}
+	else
+		_puchar(n);
+	_putchar('\n');
+}
