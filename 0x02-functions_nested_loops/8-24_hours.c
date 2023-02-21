384c4ff30be81ced5e66047566e41117cfa67be3
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-02-21 04:37:32 -0800

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print mins
+ * 
+ * Return: nada
+ *
+ */
+void jack_bauer(void)
+{
+	int minutes, hour;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minutes = 0; minutes < 61; minutes++)
+		{
+			if (hour < 10 && minutes < 10)
+			{
+				_putchar('0' + hour + ':' + '0' + minutes);
+			}
+			else
+			{
+				_putchar(hour + ':' + minutes);
+			}
+		}
+	}
