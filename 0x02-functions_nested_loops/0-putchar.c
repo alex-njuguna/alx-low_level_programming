15bc1bf7fbcbd4e60e32523762543cb34e007d5e
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-02-21 02:15:49 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,23 @@
-
 #include "main.h"
 
+
 /**
- * main - entry point
+ * main - Entry point
  *
- * Return: 0
- *
+ * Return: nothing, return void
  */
+
 int main(void)
 {
-	printf("_putchar\n");
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
 }
