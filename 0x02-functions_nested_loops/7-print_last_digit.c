33f6cc7d614524df88aa6948f441006e389d28de
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-02-21 04:16:37 -0800

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ * @i: par to be checked
+ * Return: 0
+ *
+ */
+int print_last_digit(int i)
+{
+	int n;
+
+	n = i % 10;
+	_putchar(n);
+	return (n);
+}
