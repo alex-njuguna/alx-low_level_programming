4659e7f848368d7437ee2e225f0a6192b1a07b35
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-02-22 05:38:09 -0800

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,33 +2,24 @@
 
 /**
  * fib - print 98 fibonnaci
- *@n: 98
- * Return: 0
- */
-int fib(int n)
-{
-	int i, result;
-	
-	i = 3;
-	do {
-		result = fib(i - 1) + fib(i - 2);
-		return (result);
-		i++;
-	} while (i < n);
-
-}
-
-/**
- * main - entry point
  *
  * Return: 0
  */
-int main(void)
+int main()
 {
-	int n = 98;
 
-	printf("1, ");
-	printf("2, ");
-	printf("%d, ", fib(n));
-	return (0);
+int i;
+int t1 = 1, t2 = 2, nextTerm;
+
+nextTerm = t1 + t2;
+printf("%d, %d, ", t1, t2);
+for (i = 3; i <= 98; ++i)
+{
+printf("%d, ", nextTerm);
+t1 = t2;
+t2 = nextTerm;
+nextTerm = t1 + t2;
+}
+
+return 0;
 }
