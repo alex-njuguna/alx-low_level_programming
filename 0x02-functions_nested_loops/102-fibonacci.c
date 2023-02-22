13d8f2ec4ed9be0bd8ef3468524129ec11334b46
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-02-22 20:43:14 +0000

    task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,15 +11,19 @@ int main(void)
 int i;
 int t1 = 1, t2 = 2, nextTerm;
 
-nextTerm = t1 + t2;
-printf("%d, %d, ", t1, t2);
-for (i = 3; i <= 50; ++i)
+for (i = 0; i < 50; i++)
 {
-        printf("%d, ", nextTerm);
-        t1 = t2;
-        t2 = nextTerm;
-        nextTerm = t1 + t2;
-}
+nextTerm = ti + t2;
+printf("%d", nextTerm);
+t1 = t2;
+t2 = nextTerm;
+if (i == 49)
+{
 printf("\n");
+}
+else
+{
+printf(", ");
+
 return (0);
 }
