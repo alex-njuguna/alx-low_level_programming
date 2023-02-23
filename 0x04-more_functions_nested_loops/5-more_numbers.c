c53c7a89129ca779658d7a1bfb55b48f4211d22e
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-02-23 04:40:17 -0800

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,18 +5,16 @@
  */
 void more_numbers(void)
 {
-	int i, j;
 
+int i, j;
 	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0; j < 15; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
-			{
-				_putchar('1');
-				_putchar(j % 10 + '0');
-			}
+			_putchar('1');
+			_putchar(j % 10 + '0');
+		}
 			_putchar('\n');
 		}
-	}
 }
