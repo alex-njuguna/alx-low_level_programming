8ad97ad4774a0044656dde7aab91195e14b2fb27
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-02-23 04:47:24 -0800

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,14 +7,14 @@
 void print_square(int size)
 {
 	int i, j;
+
 	if (size > 0)
 	{
-		for (i = 0; i <= size; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j <= size; j++)
+			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
-				_putchar('\n');
 			}
 			_putchar('\n');
 		}
