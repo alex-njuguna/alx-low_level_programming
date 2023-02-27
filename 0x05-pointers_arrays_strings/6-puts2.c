fa4cd1467c86c76e42debf73be744d18bf999ff4
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-02-27 20:11:46 +0000

    mandatory tasks

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,10 @@
 */
 void puts2(char *str)
 {
-int i = 0;
+	int i;
 
-while (str[i] != '\0')
-{
-_putchar(str[i]);
-i++;
-}
-_putchar('\n');
+	for (i = 0; str[i] != '\0'; i++)
+	if (i % 2 == 0)
+		_putchar(str[i]);
+	_putchar('\n');
 }
