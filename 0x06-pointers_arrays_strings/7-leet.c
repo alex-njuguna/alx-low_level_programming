6adc6ae55277ae61bb2ca6783d6ad78d880b3821
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-03-01 16:03:45 +0000

    mandatory tasks

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,25 +1,28 @@
 #include "main.h"
 
 /**
-* leet - encode string to digits
-* @s: par
-*Return: char
-*/
+ * leet - encodes a string in 1337
+ * @s: string to be encoded
+ *
+ * Return: the resulting string;
+ */
 char *leet(char *s)
 {
-char *str1 = "aAeEoOtTlL";
-char *str2 = "4433007711";
-int i, j;
+	int i, j;
 
-for (i = 0; s[i] != '\0'; i++)
-{
-for (j = 0; j <= 9; j++)
-{
-if (str1[j] == s[i])
-{
-s[i] == str2[j];
-}
-}
-}
-return (s);
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+
+	return (s);
 }
