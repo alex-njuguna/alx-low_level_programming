ecf789ae68cee9d896fb00c7ba6806283e578e51
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-03-02 20:53:15 +0000

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -52,7 +52,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		if (i < 0)
 			val1 = 0;
 		else
-			val1 = *(n1 + i) - '0';
+			val1 = *(n1 + i) -'0';
 		if (j < 0)
 			val2 = 0;
 		else
