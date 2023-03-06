d61c5f6aee05388b5bf0356e2d88fcdc4fc125da
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-03-06 19:25:06 +0000

    advanced tasks

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- * _strpbrk - function that searches a string for any of a set of bytes
- *
+ * _strpbrk - search a string for any of a set of bytes
  * @s:first occurrence in the string
- 
  * @accept: matches one of the bytes, or @NULL if no such byte
  * Return: a pointer to the byte
  */
