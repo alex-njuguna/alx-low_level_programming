6269829f13a3b1b38f38fdc214094c59073aedc2
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-03-16 21:57:22 +0000

    all tasks

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,10 +6,12 @@
 */
 void *malloc_checked(unsigned int b)
 {
-    void *i;
+void *i;
 
-    i = malloc(b);
-    if (i == NULL)
-        exit(98);
-    return (i);
+i = malloc(b);
+if (i == NULL)
+{
+exit(98);
+}
+return (i);
 }
