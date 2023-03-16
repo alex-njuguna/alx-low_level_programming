3487f92fec80eb4cd26df0d0ec2c66207080cbb2
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-03-16 19:06:41 +0000

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*malloc_checked - allocate memory using malloc
+*@b: unsigned integer
+*/
+void *malloc_checked(unsigned int b)
+{
+    void *i;
+
+    i = malloc(b);
+    if (i == NULL)
+        exit(98);
+    return (i);
+}
