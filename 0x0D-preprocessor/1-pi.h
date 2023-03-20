74b50e28a9124c4dc827a295ebc912066689af2b
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-03-20 14:27:29 +0300

    preprocessor

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,6 @@
+#ifndef MY_HEADER_FILE_H
+#define MY_HEADER_FILE_H
+
+#define PI 3.14159265359
+
+#endif /* MY_HEADER_FILE_H */
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+    printf("This program was compiled from file: %s\n", __FILE__);
+    return (0);
+}
