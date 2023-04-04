56e302629defa8337fac98f1b4c8d3e3af1e7c9f
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-04-04 22:20:38 +0300

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free a list
+ * @head:pointer to a head pointer
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = *head;
+
+	if (head == NULL)
+		return;
+	
+	while (*head != NULL)
+	{
+		*temp = *head;
+		*head = *head->next;
+		free(temp);
+	}
+}
