652fa9f7b546a7d6094d30143a45da6b4dc5cc8e
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-04-04 17:08:56 +0300

    task o

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,6 +10,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t nodeNumber = 0;
 
+	if (h -> NULL)
+	{
+		printf("no elements in the list");
+		return (-1);
+	}
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -16,8 +16,8 @@ size_t listint_len(const listint_t *h)
 
 	while (h->next != NULL)
 	{
-		h = h->next;
 		count++;
+		h = h->next;
 	}
 	return (count);
 }
