80362b5039cd9025b8454a13249e449dd6599b1e
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-04-04 14:49:57 +0300

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+#include "lists.h"
+
+/**
+ * print_listint - Prints all the elements of a listint_t list.
+ * @h: A pointer to the listint_t list.
+ * Return: The number of nodes in the listint_t list.
+ */
+size_t print_listint(const listint_t *h)
+{
+size_t count = 0;
+
+while (h != NULL)
+{
+int num = h->n;
+
+if (num < 0)
+{
+_putchar('-');
+num = -num;
+}
+
+if (num / 10)
+{
+print_listint((const listint_t *)(num / 10));
+}
+
+_putchar(num % 10 + '0');
+
+h = h->next;
+count++;
+}
+
+_putchar('\n');
+
+return (count);
+}
