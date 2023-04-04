9db17f384b8ac4abc82043b57a379cf1fa8b2de8
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-04-04 15:20:53 +0300

    task 0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,9 +1,6 @@
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
+#ifndef LISTS_H
+#define LISTS_H
 
-#ifndef lists.h
-#define lists.h
 
 /**
  * struct listint_s - singly linked list
@@ -11,7 +8,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
+ * 
  */
 typedef struct listint_s
 {
@@ -19,18 +16,10 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-
-/*prototypes*/
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
-void free_listint2(listint_t **head);
-int pop_listint(listint_t **head);
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
-int sum_listint(listint_t *head);
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
-int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
 #endif
