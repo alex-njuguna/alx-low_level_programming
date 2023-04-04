0162ae34665f6d55b796898afdd41451262e872b
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-04-04 15:07:41 +0300

    task 0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -21,7 +21,6 @@ typedef struct listint_s
 
 
 /*prototypes*/
-int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
