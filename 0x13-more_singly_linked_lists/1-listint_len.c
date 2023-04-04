8c55d76aea59794d4790f8c73c789c8c4da3f99c
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-04-04 16:33:33 +0300

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * listint_len - return num of elements in a list
+ * @h: pointer to the listint_t
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (h->next == NULL)
+	{
+		return (count);
+	}
+
+	while (h->next != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
