3db79581da53265b232d0350d40058b342bfd106
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-04-04 18:30:27 +0300

    task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,7 +8,9 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t newNode = (listint_t *) malloc(sizeof(listint_t));
+	listint_t *newNode;
+
+	newNode = malloc(sizeof(listint_t));
 	
 	if (newNode == NULL)
 		return (NULL);
