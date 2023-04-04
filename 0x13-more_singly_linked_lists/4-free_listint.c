29a5d169c2e5e7c05589031cad9218cdeb4ccdbf
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-04-04 20:47:35 +0300

    task 5

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - delete all nodes
+ * @head: pointer to head
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = *head->next;
+		free(temp)
+	}
+}
