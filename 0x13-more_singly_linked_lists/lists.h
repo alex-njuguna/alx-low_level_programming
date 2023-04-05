efc1d9bd92cfc01f905167e8048ccc3c07c31c5a
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-04-05 12:21:09 +0300

    updates lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -8,9 +8,7 @@
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
- *
  * Description: singly linked list node structure
- * 
  */
 typedef struct listint_s
 {
