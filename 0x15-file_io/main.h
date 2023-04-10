2a3f8e8b3966dff5a3d80d269e09945b6cbf08df
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-04-11 02:01:30 +0300

    updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
-
-#ifndef MAIN_H
-#define MAIN_H
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
