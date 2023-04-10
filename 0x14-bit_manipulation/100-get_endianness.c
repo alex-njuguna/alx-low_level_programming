6894e14ab772d89aeb7db50ad964d723eb08e45f
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-04-10 14:02:07 +0300

    check endianess

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,1 +1,16 @@
 #include "main.h"
+
+/**
+ * get_endianness - as the bname suggests
+ * Return: 0 or 1
+ */
+int get_endianness(void)
+{
+	int i;
+        char *c;
+
+        i = 1;
+        c = (char *)&i;
+
+        return (*c);
+}
