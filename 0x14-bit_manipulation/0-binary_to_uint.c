8d4555d6c05f8b852f3bc63ad7484c5d56a854d5
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-04-10 13:58:26 +0300

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,15 +13,17 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	for (i = 0; b[i] != '\0'; i++)
+	while (b[i])
 	{
-		if (b[i] == '0')
+		if (b[i] == '0' || b[i] == '1')
+		{
 			result = result << 1;
-		else if (b[i] == '1')
-			result = (result <<= 1) |= 1;
+			if (b[i] == '1')
+				result = result | 1;
+			i++;
+		}
 		else
 			return (0);
 	}
-
 	return (result);
 }
