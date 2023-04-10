6b14c7fa5105d3c7184ebd980a67fa20867ff368
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-04-11 01:44:31 +0300

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * read_textfile - read a text file and prints it
+ * @filename: pointer to the file name
+ * letters:params
+ * Return:no of letters
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int read_count, fd, write_count;
+	char *buffer;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+		return (0);
+
+	read_count = read(fd, buffer, letters);
+	if (read_count == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	write_count = write(STDOUT_FILENO, buffer, read_count);
+	free(buffer);
+	close(fd);
+	
+	if (read_count != write_count)
+		return (0);
+
+	return (write_count);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <fcntl.h>
+
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+
+#endif
