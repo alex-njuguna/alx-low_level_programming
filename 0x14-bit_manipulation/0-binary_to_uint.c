db485bc5f2ece211838497d77a0c1a9181715e9d
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-04-10 14:36:39 +0300

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,13 +10,17 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int result = 0;
 	int i = 0;
 
-	if (b[i] == '\0')
+	if (b == NULL)
 		return (0);
 
-	while ((b[i] == '0') || (b[i] == '1'))
+	while (b[i] != '\0')
 	{
-		result = result << 1;
-		result = result | (b[i] - '0');
+		if (b[i] == '0')
+			result = result << 1;
+		else if (b[i] == '1')
+			result = (result << 1) | 1;
+		else
+			return (0)
 		i++;
 	}
 	return (result);
