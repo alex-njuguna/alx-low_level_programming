e6f507d36a663fb84018f907840ca328e58a4e3b
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-04-11 05:43:07 +0300

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,113 +1,58 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
-#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUF_SIZE 1024
 
 /**
- * read_file_content - reads the content of a file
- * @filename: pointer to file name
- * Return: pointer to a buffer
+ * error_exit - shows an error message
+ * @message: error message
+ * @exit_code: exit code
  */
-char *read_file_content(const char *filename)
+void error_exit(char *message, int exit_code)
 {
-	int fd, n_read, size = 1024, total = 0;
-	char *buf = malloc(size);
-
-	if (!buf)
-		return (NULL);
-
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (NULL);
-
-	while ((n_read = read(fd, buf + total, size - total)))
-	{
-		if (n_read == -1)
-			return (NULL);
-		total += n_read;
-		if (total >= size)
-		{
-			size += 1024;
-			buf = realloc(buf, size);
-			if (!buf)
-				return (NULL);
-		}
-	}
-	if (close(fd) == -1)
-		return (NULL);
-
-	buf = realloc(buf, total + 1);
-	if (!buf)
-		return (NULL);
-
-	buf[total] = '\0';
-
-	return (buf);
+	dprintf(STDERR_FILENO, "%s\n", message);
+	exit(exit_code);
 }
 
 /**
- * write_file_content - writes the content of a buffer to a file
- * @filename: pointer to file name
- * @buf: buffer containing the file content
- * @len: size of the buffer
- * Return: 1 or -1
- */
-int write_file_content(const char *filename, char *buf, int len)
-{
-	int fd, n_write, total = 0;
-
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd == -1)
-		return (-1);
-
-	while (total < len)
-	{
-		n_write = write(fd, buf + total, len - total);
-		if (n_write == -1)
-			return (-1);
-		total += n_write;
-	}
-
-	if (close(fd) == -1)
-		return (-1);
-
-	return (1);
-}
-
-
-/**
- * main- entry point
+ * main - entry point
  * @argc: count
  * @argv: storage
- * Return: 0
+ * Return: 0, non zero
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	char *buf;
-	int len, ret;
+	int fd_from, fd_to, n_read, n_write;
+	char buf[BUF_SIZE];
 
 	if (argc != 3)
+		error_exit("Usage: cp file_from file_to", 97);
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		error_exit("Error: Can't read from file", 98);
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+		error_exit("Error: Can't write to file", 99);
+
+	while ((n_read = read(fd_from, buf, BUF_SIZE)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		n_write = write(fd_to, buf, n_read);
+		if (n_write == -1)
+			error_exit("Error: Can't write to file", 99);
 	}
 
-	buf = read_file_content(argv[1]);
-	if (!buf)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+	if (n_read == -1)
+		error_exit("Error: Can't read from file", 98);
 
-	len = strlen(buf);
-	ret = write_file_content(argv[2], buf, len);
-	if (ret == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+	if (close(fd_from) == -1)
+		error_exit("Error: Can't close fd", 100);
 
-	free(buf);
-	exit(0);
+	if (close(fd_to) == -1)
+		error_exit("Error: Can't close fd", 100);
+
+	return (0);
 }
