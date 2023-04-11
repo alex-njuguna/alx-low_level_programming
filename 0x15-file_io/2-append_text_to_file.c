b4a4243baf6a9d761848f31214bcd0d961aeb992
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-04-11 03:01:00 +0300

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_context)
+	if (text_content)
 	{
 		while (text_content[len])
 			len++;
