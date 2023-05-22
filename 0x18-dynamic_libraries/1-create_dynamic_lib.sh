317c8692d6150d1d9c51f63968e9f06f3bc5ac6d
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-05-23 01:24:30 +0300

    dynamic libraries

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-gcc -fPIC -c *.c
+
+c_files=$(find . -name "*.c" -type f)
+
+for file in $c_files; do
+    gcc -c -Wall -fpic "$file" -o "${file%.c}.o"
+done
+
 gcc -shared -o liball.so *.o
+
+rm -f *.o
